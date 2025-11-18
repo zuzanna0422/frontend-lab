e9677f2fde7fad88ae952ac13deee3ef65b221ab
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2025-11-18 13:32:35 +0100

    Moved files directly to frontend-lab

---
+++ src/module-data.js
@@ -0,0 +1,37 @@
+export const people = [
+    {
+        "id": 1,
+        "name": "Ania",
+        "birthDate": "1986-03-28",
+        "email": "ania1@wsei.edu.pl",
+        "phone": "294-503-459"
+    },
+    {
+        "id": 2,
+        "name": "Wiktoria",
+        "birthDate": "1996-05-26",
+        "email": "wiktoria1@wsei.edu.pl",
+        "phone": "287-538-862"
+    },
+    {
+        "id": 3,
+        "name": "Kasia",
+        "birthDate": "1980-04-21",
+        "email": "kasia1@wsei.edu.pl",
+        "phone": "251-510-061"
+    },
+    {
+        "id": 4,
+        "name": "Zuzia",
+        "birthDate": "1990-05-17",
+        "email": "zuzia1@wsei.edu.pl",
+        "phone": "240-691-619"
+    },
+    {
+        "id": 5,
+        "name": "Zuzia",
+        "birthDate": "1988-10-21",
+        "email": "zuzia2@wsei.edu.pl",
+        "phone": "246-780-103"
+    }
+];
